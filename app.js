ea5ba3053f7a398bffa018f60bc1b2c1e056f336
mateobriggs <mateobriggs@gmail.com>
2022-10-19 19:22:11 -0300

    Subi mi proyecto a Git Hub

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+
+const path = require('path')
+
+const publicPath = path.resolve('public')
+app.use(express.static(publicPath))
+
+app.listen(3000)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'))
+})
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve('views/register.html'))
+})
+
+app.get('/ingreso', (req, res) => {
+    res.sendFile(path.resolve('views/login.html'))
+})
+
+app.post('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'))
+})
